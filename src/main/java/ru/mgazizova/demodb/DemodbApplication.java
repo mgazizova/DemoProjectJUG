2e458b9058211fe80bfdaf537dc1ac131b7da91e
mgazizova <mgazizova@naumen.ru>
2021-09-29 16:47:35 +0500

    fix

--- src/main/java/ru/mgazizova/demodb/DemodbApplication.java
+++ src/main/java/ru/mgazizova/demodb/DemodbApplication.java
@@ -1,12 +1,14 @@
 package ru.mgazizova.demodb;
 
 import org.springframework.boot.SpringApplication;
+import java.lang.Exception;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemodbApplication {
 
 	public static void main(String[] args) {
+		int i;
 		SpringApplication.run(DemodbApplication.class, args);
 	}
 }
